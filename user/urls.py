23fa831bf5901d20a77e09bd92ff2c81b8b8f251
skiboorg <d@skib.org>
2021-09-02 21:52:20 +0300

    add first use promo

--- user/urls.py
+++ user/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('add_address', views.AddAddress.as_view()),
     path('delete_address/<int:pk>', views.DeleteAddress.as_view()),
     path('use_promo', views.UsePromo.as_view()),
-    path('send_code_sms', views.SendCodeSMS.as_view()),
+    path('comfirm_phone_step_one', views.ComfirmPhoneStepOne.as_view()),
     path('change_password', views.ChangePassword.as_view()),
     path('set_n_id', views.SetNId.as_view()),
 
