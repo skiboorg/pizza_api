715e6433057b37bdf9eb4d9d943a384d8d8b3715
skiboorg <d@skib.org>
2021-11-24 14:20:43 +0300

    add first use promo

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
-admin.site.register(User)
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('phone',)
+    ordering = ('id',)
+    search_fields = ('phone',)
+admin.site.register(User,UserAdmin)
 admin.site.register(Guest)
 admin.site.register(UserAddress)
 admin.site.register(Promo)
