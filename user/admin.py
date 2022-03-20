ab5f92442d3f7a2b84b42159b5d798b902dbdc78
skiboorg <d@skib.org>
2022-03-20 11:24:00 +0300

    add first use promo

--- user/admin.py
+++ user/admin.py
@@ -7,6 +7,26 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('phone',)
     ordering = ('id',)
     search_fields = ('phone',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone',  'password1', 'password2'),
+        }),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info',
+         {'fields': (
+             'phone',
+             'city',
+             'promo',
+             'fio',
+             'bonuses',
+
+         )}
+         ),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser',)}),)
+
 admin.site.register(User,UserAdmin)
 admin.site.register(Guest)
 admin.site.register(UserAddress)
